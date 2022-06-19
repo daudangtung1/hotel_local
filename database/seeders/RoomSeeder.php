5feb08486beba2b805a0a2c19b15d06031528667
nguyen.quy <nguyen.quy>
2022-06-19 23:00:22 +0700

    add route

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -18,30 +19,34 @@ class RoomSeeder extends Seeder
      */
     public function run()
     {
-        for($i = 0; $i <= 10; $i++) {
-            $room = Room::create([
-                'name' => 'Phòng số ' . $i,
-                'status' => 0,
-                'floor' => 'Tầng ' . rand(1, 5),
-                'hour_price' => rand(10, 20) . '000',
-                'day_price' => rand(20, 30) . '0000',
-            ]);
-//
-//            for($j = 0; $j <= 20; $j++) {
-//                DB::table('booking_rooms')->insert([
-//                    'room_id' => $room->id,
-//                    'start_date' => Carbon::now(),
-//                    'end_date' => Carbon::now()->addHour(rand(1, 20)),
-//                ]);
-//            }
-        }
-        for ($j = 0; $j <= 5; $j++) {
-            DB::table('services')->insert([
-                'name'  => 'Dịch vụ ' . rand(1, 10),
-                'stock' => rand(10, 50),
-                'price' => rand(10, 20) . '000',
-                'type'  => rand(0, 1),
-            ]);
-        }
+        // User::create(['name'=>'admin', 'email'=>'admin@gmail.com', 'email_verified_at'=> now(), 'password'=> bcrypt('admin@#')]);
+        User::create(['name'=>'admin', 'email'=>'guest@gmail.com', 'email_verified_at'=> now(), 'password'=> bcrypt('guest@#')]);
+        User::create(['name'=>'admin', 'email'=>'staff@gmail.com', 'email_verified_at'=> now(), 'password'=> bcrypt('staff@#')]);
+
+//         for($i = 0; $i <= 10; $i++) {
+//             $room = Room::create([
+//                 'name' => 'Phòng số ' . $i,
+//                 'status' => 0,
+//                 'floor' => 'Tầng ' . rand(1, 5),
+//                 'hour_price' => rand(10, 20) . '000',
+//                 'day_price' => rand(20, 30) . '0000',
+//             ]);
+// //
+// //            for($j = 0; $j <= 20; $j++) {
+// //                DB::table('booking_rooms')->insert([
+// //                    'room_id' => $room->id,
+// //                    'start_date' => Carbon::now(),
+// //                    'end_date' => Carbon::now()->addHour(rand(1, 20)),
+// //                ]);
+// //            }
+//         }
+//         for ($j = 0; $j <= 5; $j++) {
+//             DB::table('services')->insert([
+//                 'name'  => 'Dịch vụ ' . rand(1, 10),
+//                 'stock' => rand(10, 50),
+//                 'price' => rand(10, 20) . '000',
+//                 'type'  => rand(0, 1),
+//             ]);
+//         }
     }
 }
