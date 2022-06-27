904f30d97a95a7a284c108c094219a7f9de49e3a
nguyen.quy <nguyen.quy>
2022-06-27 22:24:21 +0700

    export

---
+++ app/Http/Controllers/Reportcontroller.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exports\BookingRoomExport;
+use App\Repositories\BookingRoomRepository;
+use App\Repositories\RoomRepository;
+use App\Repositories\ServiceRepository;
+use App\Repositories\TypeRoomRepository;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Excel;
+
+class ReportController extends Controller
+{
+    public $roomRepository;
+
+    public $serviceRepository;
+
+    public $typeRoomRepository;
+
+    public $excel;
+
+    public function __construct(Excel $excel, TypeRoomRepository $typeRoomRepository, RoomRepository $roomRepository, ServiceRepository $serviceRepository, BookingRoomRepository $bookingRoomRepository)
+    {
+        $this->excel = $excel;
+        $this->roomRepository = $roomRepository;
+        $this->typeRoomRepository = $typeRoomRepository;
+        $this->serviceRepository = $serviceRepository;
+        $this->bookingRoomRepository = $bookingRoomRepository;
+    }
+
+    public function index(Request $request)
+    {
+        if(!empty($request->export)) {
+            return $this->excel->download(new BookingRoomExport($request), 'booking-rooms.xlsx');
+        }
+
+        $bookingRooms = $this->bookingRoomRepository->filter($request);
+        return view('report.index', compact('bookingRooms'));
+    }
+}
---
+++ resources/views/report/index.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.app')
+@section('content')
+    <div class="wrap-main">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h5>Báo cáo</h5>
+                        <div class="filter">
+                            <form action="{{route('reports.index')}}" class="d-flex" method="GET">
+                                <input type="text" name="s" id="s" class="form-control me-2" placeholder="Từ khóa" value="@if(!empty(request()->s)) {{request()->s}} @endif">
+                                <select class="form-control me-2" name="status">
+                                    <option value="">Loại phòng</option>
+                                    @foreach(\App\Models\Room::ARRAY_STATUS as $key => $status)
+                                            <option @if(!empty(request()->status) && request()->status == $key) selected @endif value="{{$key}}">{{$status}}</option>
+                                    @endforeach
+                                </select>
+                                <input type="text" class="form-control me-2 filter-date" placeholder="Ngày bắt đầu" name="start_date" value="@if(!empty(request()->start_date)) {{request()->start_date}} @endif"  autocomplete="false">
+                                <input type="text" class="form-control me-2 filter-date" placeholder="Ngày kết thúc" name="end_date" value="@if(!empty(request()->end_date)) {{request()->end_date}} @endif" autocomplete="false">
+                                <button class="btn btn-success me-2">Lọc</button>
+                                <button class="btn btn-danger" type="submit" name="export" value="export">Xuất Excel</button>
+                            </form>
+                        </div>
+                    </div>
+                    <table class="table table-sm table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Tên phòng</th>
+                            <th scope="col">Tầng</th>
+                            <th scope="col">Ngày nhận phòng</th>
+                            <th scope="col">Ngày trả phòng</th>
+                            <th scope="col">Tên khách hàng</th>
+                            <th scope="col">Ghi chú</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @if(!empty($bookingRooms))
+                            @forelse($bookingRooms as $key => $bookingRoom)
+                                <tr>
+                                    <td>{{$bookingRoom->id}}</td>
+                                    <td>{{$bookingRoom->room->name ?? '-'}}</td>
+                                    <td>{{$bookingRoom->room->floor ?? '-'}}</td>
+                                    <td>{{$bookingRoom->start_date ?? '-'}}</td>
+                                    <td>{{$bookingRoom->end_date ?? '-'}}</td>
+                                    <td>
+                                        @foreach($bookingRoom->bookingRoomCustomers()->get() as $customer)
+                                            <p>{{$customer->customer->name ?? '-'}}</p>
+                                        @endforeach
+                                    </td>
+                                    <td>{{$bookingRoom->note ?? '-'}}</td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="7">Không có phòng nào</td>
+                                </tr>
+                            @endforelse
+                        @else
+                            <tr>
+                                <td colspan="7">Không có phòng nào</td>
+                            </tr>
+                        @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('script')
+    <script>
+        $(document).ready(function () {
+            var date = $('.filter-date');
+            if (date) {
+                date.datetimepicker({
+                    todayHighlight: true,
+                    format: 'Y-m-d',
+                    startDate: new Date()
+                });
+            }
+        });
+    </script>
+@endsection
