7810905a9b96dc67c0393daaa8028fe83050b51d
daudangtung1 <65516018+daudangtung1@users.noreply.github.com>
2022-10-29 04:57:13 +0700

    Fix room front-end done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -415,10 +415,10 @@
             var debounce = null;
 
             $('body').on('keyup', 'input[name="customer_name"]', function(e) {
-              
+
                 var _this = $(this);
                 var form = _this.closest('.form-user');
-                console.log(form.find('#customer_id_card').val(),form.find('#customer_phone').val());
+                console.log(form.find('#customer_id_card').val(), form.find('#customer_phone').val());
                 if (form.find('#customer_id_card').val() != '' && form.find('#customer_phone').val() != '') {
                     return false;
                 }
@@ -1422,10 +1422,10 @@
                     area: $('#select-area').val(),
                     order_by: $('#select-order').val()
                 },
-                success: function (data) {
-                  $('#room-list').html('').html(data)
+                success: function(data) {
+                    $('#room-list').html('').html(data)
                 },
-                error: function (e) {
+                error: function(e) {
                     console.log(e);
                 }
             })
@@ -1437,20 +1437,20 @@
             $.ajax({
                 type: "get",
                 url: roomUrl,
-                success: function (data) {
-                  $('#poup-booking-room').html('').html(data);
-                  $('#poup-booking-room .modal').modal('show');
-                  removeStopButton();
+                success: function(data) {
+                    $('#poup-booking-room').html('').html(data);
+                    $('#poup-booking-room .modal.modal_parent').modal('show');
+                    removeStopButton();
                 },
-                error: function (e) {
+                error: function(e) {
                     console.log(e);
                     removeStopButton();
-                    $('#poup-booking-room .modal').modal('hide');
+                    $('#poup-booking-room .modal.modal_parent').modal('hide');
                 }
             })
         });
 
-        $('.modal').on('hide.bs.modal', function (e) {
+        $('.modal.modal_parent').on('hide.bs.modal', function(e) {
             console.log('Modal close event.')
             $(this).closest('#poup-booking-room').html('');
         });
@@ -1474,7 +1474,7 @@
                     removeStopButton();
                 }
             })
-        })
+        });
 
         function getParamsFilter() {
             var area = $('#list-floor .item-active').data('value');
--- resources/views/room/modal-room.blade.php
+++ resources/views/room/modal-room.blade.php
@@ -1,12 +1,21 @@
-<div class="modal fade" id="booking-modal-{{$room->id}}" aria-labelledby="booking-modalLabel" aria-hidden="true">
+@if($room->status == \App\Models\Room::HAVE_GUEST)
+<div class="modal fade modal_child" id="modal_check_form_{{$room->id}}" aria-labelledby="booking-modalLabel" aria-hidden="true">
+    @include('room.modal-content-room-check')
+</div>
+@endif
+<div class="modal fade modal_parent" id="booking-modal-{{$room->id}}" aria-labelledby="booking-modalLabel" aria-hidden="true">
     <input type="hidden" name="room_id" value="{{$room->id}}">
     @include('room.modal-content-room')
 </div>
-
 <script>
     $('.datetime-picker').datetimepicker({
         todayHighlight: true,
         format: 'Y-m-d H:i',
         startDate: new Date()
+    });
+
+    $('body').on('click', '#modal_check_btn_{{$room->id}}', function() {
+        $(this).parents('#poup-booking-room').find('.modal_parent').modal('hide');
+        $(this).parents('#poup-booking-room').find('.modal_child').modal('show');
     });
 </script>
